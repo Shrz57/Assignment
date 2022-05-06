e0addc7ee49dc028e96243f6a8f7a65564bb90bf
SHUSHANT SHRESTHA <53145557+Shrz57@users.noreply.github.com>
2022-05-06 18:21:53 +0545

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,15 @@
+
+def append_two_list(l1,l2):
+    for index,item in enumerate(l2):
+        if(index%2!=0):
+            l1.append(l2[index])
+    print(l1)
+
+
+list_b=[item for item in range(1,70)  if item % 2 == 0 if item % 5==0]
+list_a=[ items for items in range (1,len(list_b))]
+# list = [x for index , x in enumerate(list_b) if ind % 2!=0]
+print(list_a)
+print(list_b)
+append_two_list(list_a,list_b)
+
